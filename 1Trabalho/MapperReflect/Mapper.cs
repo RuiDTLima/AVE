169a6d4c1706a134836b51e3f3828eb76eec0d1f
rui_l <rui_lima24@hotmail.com>
2017-04-03 16:21:53 +0100

    Primeira fase, nova versão.

--- 1Trabalho/MapperReflect/Mapper.cs
+++ 1Trabalho/MapperReflect/Mapper.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Reflection;
-using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 
 namespace MapperReflect
 {
@@ -11,7 +8,7 @@ namespace MapperReflect
 
         private Type dest { get; set; }
 
-        public Mapping mapAtribute { get; private set; }
+        private Mapping mapAtribute { get; set; }
 
         private Dictionary<string, string> dict = new Dictionary<string, string>();
 
@@ -23,34 +20,13 @@ namespace MapperReflect
 
         //Falta aplicar a parte do bind e do match.
         public object Map(object src)
-        {
-            if (!src.GetType().Equals(this.src))
-                return null;
-            ArrayList p = new ArrayList();
-            PropertyInfo[] srcProperties = src.GetType().GetProperties();
-            object aux = Activator.CreateInstance(dest);
-            PropertyInfo[] destProperties = dest.GetProperties();
-            PropertyInfo destino, origem;
-
-            for (int i = 0; i < destProperties.Length; i++)
-            {
-                for (int j = 0; j < srcProperties.Length; j++) { 
-                    destino = destProperties[i];
-                    origem = srcProperties[j];
-                    if (destino.GetType().Equals(origem.GetType()) && destino.Name.Equals(origem.Name))
-                        destino.SetValue(aux, origem.GetValue(src));
-                }
-            }
-            return aux;
+        { 
+            return mapAtribute.Map(src);
         }
 
         public object[] Map(object[] src)
         {
-            object[] obj = new object[src.Length];
-            for (int i = 0; i < src.Length; ++i) {
-                obj[i] = Map(src[i]);
-            }
-            return obj;
+            return mapAtribute.Map(src);
         }
 
         public Mapper Bind(Mapping m)
--- 1Trabalho/MapperReflect/Mapping.cs
+++ 1Trabalho/MapperReflect/Mapping.cs
@@ -6,12 +6,24 @@ using System.Threading.Tasks;
 
 namespace MapperReflect
 {
-
-    class Mapping
+    class Mapping : IMapper
     {
-        internal class Properties : Attribute { }
-        internal class Fields : Attribute {}
-        
+        public virtual Type src { get; set; }
 
+        public virtual Type dest { get; set; }
+
+        public virtual object Map(object src) {
+            throw new NotImplementedException();
+        }
+
+        public object[] Map(object[] src)
+        {
+            object[] obj = new object[src.Length];
+            for (int i = 0; i < src.Length; ++i)
+            {
+                obj[i] = Map(src[i]);
+            }
+            return obj;
+        }
     }
 }
